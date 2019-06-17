be6aca4d5780421086a149a27ac6e3914cb0b0c2
Miguel Hatrick <miguelhatrick@gmail.com>
2019-06-17 20:40:07 -0300

    Update AdminLTEHelper.php

--- AdminLTEHelper.php
+++ AdminLTEHelper.php
@@ -3,7 +3,7 @@ namespace miguelhatrick\YiiToolbox;
 
 class AdminLTEHelper
 {
-    public static function startBox($title, $collapsed = true, $class = 'success') {
+    public static function startBox($title, $collapsed = true, $class = 'success', $closable = false) {
         
         /*
          * Class types:
@@ -11,6 +11,7 @@ class AdminLTEHelper
          * info
          * danger
          * primary
+         * warning
          */
         
         $html = <<< EOT_MY_CODE
@@ -22,17 +23,23 @@ class AdminLTEHelper
             data-widget="collapse">
             <i class="fa fa-SIGN"></i>
             </button>
-            
+            CLOSABLE
             </div>
             </div>
             
             <div class="box-body">
 EOT_MY_CODE;
+        $closablehtml = '<button type="button" class="btn btn-box-tool" data-widget="remove"><i class="fa fa-times"></i></button>';
+        
         $html = preg_replace("(TITLE)", $title, $html);
         $html = preg_replace("(COLLAPSED)", $collapsed ? 'collapsed-box' : '', $html);
-        $html = preg_replace("(SIGN)", $collapsed ? 'plus' : 'minux', $html);
+        $html = preg_replace("(SIGN)", $collapsed ? 'plus' : 'minus', $html);
         
         $html = preg_replace("(CLASS)", $class, $html);
+        $html = preg_replace("(CLOSABLE)", $closable ? $closablehtml : '', $html);
+        
+        
+        
         
         return $html; // return the generated password
     }
