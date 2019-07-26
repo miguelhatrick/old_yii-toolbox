23427e123ce8b4c8565488d61441b61421ab7176
Miguel Hatrick <miguelhatrick@gmail.com>
2019-07-26 01:40:38 -0300

    now allows no collapse

--- AdminLTEHelper.php
+++ AdminLTEHelper.php
@@ -3,7 +3,9 @@ namespace miguelhatrick\YiiToolbox;
 
 class AdminLTEHelper
 {
-    public static function startBox($title, $collapsed = true, $class = 'success', $closable = false) {
+
+    public static function startBox($title, $collapsed = true, $class = 'success', $closable = false, $collapsable = true)
+    {
         
         /*
          * Class types:
@@ -13,48 +15,34 @@ class AdminLTEHelper
          * primary
          * warning
          */
-        
         $html = <<< EOT_MY_CODE
-       <div class="box box-CLASS COLLAPSED">
-        <div class="box-header">
+<div class="box box-CLASS COLLAPSED">
+    <div class="box-header">
         <h3 class="box-title">TITLE</h3>
         <div class="box-tools pull-right">
-        <button type="button" class="btn btn-box-tool"
-            data-widget="collapse">
-            <i class="fa fa-SIGN"></i>
-            </button>
+            COLLAPSABLE
             CLOSABLE
-            </div>
-            </div>
-            
-            <div class="box-body">
+        </div>
+    </div>
+<div class="box-body">
 EOT_MY_CODE;
         $closablehtml = '<button type="button" class="btn btn-box-tool" data-widget="remove"><i class="fa fa-times"></i></button>';
+        $collapsablehtml = '<button type="button" class="btn btn-box-tool" data-widget="collapse"> <i class="fa fa-SIGN"></i> </button>';
         
+        $html = preg_replace("(COLLAPSABLE)", $collapsable ? $collapsablehtml : '', $html); // this goes first
         $html = preg_replace("(TITLE)", $title, $html);
         $html = preg_replace("(COLLAPSED)", $collapsed ? 'collapsed-box' : '', $html);
         $html = preg_replace("(SIGN)", $collapsed ? 'plus' : 'minus', $html);
-        
         $html = preg_replace("(CLASS)", $class, $html);
         $html = preg_replace("(CLOSABLE)", $closable ? $closablehtml : '', $html);
         
-        
-        
-        
         return $html; // return the generated password
     }
-    
-    
-    public static function endBox() {
-        
 
-        
+    public static function endBox()
+    {
         return '</div>
                 </div>';
     }
-    
-    
-    
-    
 }
 
