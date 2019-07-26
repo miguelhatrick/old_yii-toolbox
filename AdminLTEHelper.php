7057e14263f3d6c116e5d52d2ed58307e284be78
Miguel Hatrick <miguelhatrick@gmail.com>
2019-07-26 07:11:18 -0300

    Update AdminLTEHelper.php

--- AdminLTEHelper.php
+++ AdminLTEHelper.php
@@ -3,9 +3,7 @@ namespace miguelhatrick\YiiToolbox;
 
 class AdminLTEHelper
 {
-
-    public static function startBox($title, $collapsed = true, $class = 'success', $closable = false, $collapsable = true)
-    {
+    public static function startBox($title = '', $collapsed = true, $class = 'success', $closable = false, $collapsable = true) {
         
         /*
          * Class types:
@@ -15,10 +13,11 @@ class AdminLTEHelper
          * primary
          * warning
          */
+        
         $html = <<< EOT_MY_CODE
 <div class="box box-CLASS COLLAPSED">
     <div class="box-header">
-        <h3 class="box-title">TITLE</h3>
+        TITLEPOSITION
         <div class="box-tools pull-right">
             COLLAPSABLE
             CLOSABLE
@@ -26,10 +25,13 @@ class AdminLTEHelper
     </div>
 <div class="box-body">
 EOT_MY_CODE;
+        $titlehtml = '<h3 class="box-title">TITLE</h3>';
         $closablehtml = '<button type="button" class="btn btn-box-tool" data-widget="remove"><i class="fa fa-times"></i></button>';
         $collapsablehtml = '<button type="button" class="btn btn-box-tool" data-widget="collapse"> <i class="fa fa-SIGN"></i> </button>';
         
+        
         $html = preg_replace("(COLLAPSABLE)", $collapsable ? $collapsablehtml : '', $html); // this goes first
+        $html = preg_replace("(TITLEPOSITION)", !empty($title) ? $titlehtml : '', $html); // this goes first
         $html = preg_replace("(TITLE)", $title, $html);
         $html = preg_replace("(COLLAPSED)", $collapsed ? 'collapsed-box' : '', $html);
         $html = preg_replace("(SIGN)", $collapsed ? 'plus' : 'minus', $html);
@@ -38,11 +40,10 @@ EOT_MY_CODE;
         
         return $html; // return the generated password
     }
-
-    public static function endBox()
-    {
+    
+    
+    public static function endBox() {
         return '</div>
                 </div>';
     }
 }
-
